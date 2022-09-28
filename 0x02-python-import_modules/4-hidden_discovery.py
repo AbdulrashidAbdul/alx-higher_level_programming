077af8ccfdbd09e250030747ed3ce59d02146543
Abdulrashid Abdulrazaq <abdulrazaqabdulrashid@gmail.com>
2022-09-28 09:01:48 -0700

    Task 4.Hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-import hidden_4 as h
-
-
-def discover():
-name = dir(h)
-for i in name:
-if i[:2] != '__':
-print("{:s}".format(i))
-
-
 if __name__ == "__main__":
-discover()
+    import hidden_4
+    name = dir(hidden_4)
+    for i in range(len(name)):
+        if name[i][0] != "_" and name[i][1] != "_":
+            print(name[i])
